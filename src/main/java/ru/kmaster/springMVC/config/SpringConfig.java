dc4b421cc845b26265b80e7d17252c89c37312d5
Krylov <keriesa13@gmail.com>
2022-01-16 14:41:44 +0400

    Простейшая работа с листом-базой данных с помощью паттерна DAO

--- src/main/java/ru/kmaster/springMVC/config/SpringConfig.java
+++ src/main/java/ru/kmaster/springMVC/config/SpringConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.Thymeleaf;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
---
+++ src/main/java/ru/kmaster/springMVC/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ru.kmaster.springMVC.dao;
+
+import org.springframework.stereotype.Component;
+import ru.kmaster.springMVC.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Kirill"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Max"));
+        people.add(new Person(++PEOPLE_COUNT, "Danil"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
