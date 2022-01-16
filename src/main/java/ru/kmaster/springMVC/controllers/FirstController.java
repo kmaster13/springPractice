44c2a2fe786b678ffaa255d96267f228b54b0248
Krylov <keriesa13@gmail.com>
2022-01-16 12:59:31 +0400

    Работа с моделями и создание простого веб-калькулятора, использующий значения из GET запроса.

--- src/main/java/ru/kmaster/springMVC/controllers/FirstController.java
+++ src/main/java/ru/kmaster/springMVC/controllers/FirstController.java
@@ -1,21 +1,21 @@
 package ru.kmaster.springMVC.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name",required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -23,5 +23,33 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a", required = false) double a,
+                                 @RequestParam(value = "b", required = false) double b,
+                                 @RequestParam(value = "action", required = false) String action,
+                                 Model model) {
+
+        model.addAttribute("message", "Your numbers " + a + " and " + b + ", action is " + action);
+
+        switch (action) {
+            case "multiplication":
+                model.addAttribute("action", a * b);
+                break;
+            case "addition":
+                model.addAttribute("action", a + b);
+                break;
+            case "subtraction":
+                model.addAttribute("action", a - b);
+                break;
+            case "division":
+                model.addAttribute("action", a / b);
+                break;
+            default:
+                model.addAttribute("action", "Action is not found!");
+                break;
+        }
+        return "/first/calculator";
     }
 }
