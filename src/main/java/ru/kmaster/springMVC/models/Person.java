9440c5f5ff152415783bcdcb57b29e1a7bf9f866
Krylov <keriesa13@gmail.com>
2022-01-17 17:33:30 +0400

    Добавление POST запроса, с созданием нового человека

--- src/main/java/ru/kmaster/springMVC/models/Person.java
+++ src/main/java/ru/kmaster/springMVC/models/Person.java
@@ -17,13 +17,15 @@ public class Person {
         return name;
     }
 
-    public void setName(String name
-    ) {
+    public void setName(String name) {
         this.name = name;
     }
 
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Person(){
     }
 }
