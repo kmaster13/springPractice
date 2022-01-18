9c0ec65d6c18a33d809e01140b9d6e5d65c8f027
Krylov <keriesa13@gmail.com>
2022-01-19 01:32:19 +0400

    Добавление валидации(остался баг с редактированием созданного человека, создаётся новый)

--- src/main/java/ru/kmaster/springMVC/models/Person.java
+++ src/main/java/ru/kmaster/springMVC/models/Person.java
@@ -1,10 +1,24 @@
 package ru.kmaster.springMVC.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
 
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @NotEmpty(message = "Age should not be empty")
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be  empty")
+    @Email(message = "Email should be valid")
     private String email;
 
     public int getId() {
